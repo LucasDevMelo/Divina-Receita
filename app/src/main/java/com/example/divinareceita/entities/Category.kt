14d66df8cb621e608f3be50883b44d3eb6b30848
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-05-05 08:40:45 -0300

    Atualizacao dia 05/05 1.5

--- app/src/main/java/com/example/divinareceita/entities/Category.kt
+++ app/src/main/java/com/example/divinareceita/entities/Category.kt
@@ -1,4 +1,11 @@
 package com.example.divinareceita.entities
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
-data class Category ()
+
+data class Category (
+    @Expose
+    @SerializedName("categories")
+    val categoriesitems: List<CategoryItems>
+)
